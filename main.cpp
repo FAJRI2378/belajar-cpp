bcb9ec2614694fae0787dd520c355fdec3b2f503
FAJRI2378 <armanfajri008@gmail.com>
2026-08-26 21:09:59 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main (){
+    cout << "hello" << endl;
+    cout << endl; //space
+    cout << 123;
+    cout << endl;
+    cout << " Nama Saya =";
+    cout << "Arman Tri Fajri" << endl <<endl;
+    cout << endl;
+    cout << "NIM =";
+    cout << "12345678910";
+    cout << endl;
+    cout << "Saya maba PNJ" << endl;
+    int usia = 18;
+    usia = 19;
+    cout<< "Usia = " << usia << endl;
+    const float phi = 3.14;
+    cout << "nilai phi = " << phi << endl;
+    return 0;  
+}
---
+++ mtk.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+ int x = 10;
+ int y = 20;
+ int z = x + y;
+ cout << "nilai x di tambah nilai y = " << x << "+" << y <<"=" << z << endl;
+
+ const int a = 5;
+ const int b = 2;
+ int c = a * b; 
+ cout << "hasil penjumlahan antara nilai " << a << " dikali dengan nilai " << b << " adalah = " << c; 
+}
