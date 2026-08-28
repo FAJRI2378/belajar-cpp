e7addedb21c3aa401a71a9c11592c619eaaff2b9
FAJRI2378 <armanfajri008@gmail.com>
2026-08-28 21:42:22 +0700

    refactor: consolidate main function and improve code structure

--- main.cpp
+++ main.cpp
@@ -21,3 +21,18 @@ int main (){
     cout << "nilai phi = " << phi << endl;
     return 0;  
 }
+int main() {
+    int x = 10;
+    int y = 20;
+    int z = x + y;
+
+    x = 20;
+
+    cout << "nilai x di tambah nilai y = " << x << "+" << y <<"=" << z << endl;
+
+    const int a = 5;
+    const int b = 2;
+    int c = a * b; 
+    cout << "hasil penjumlahan antara nilai " << a << " dikali dengan nilai " << b << " adalah = " << c; 
+
+}
