3e86766e67ea1b7c8af9951a8c825afe0cf1d35b
FAJRI2378 <armanfajri008@gmail.com>
2026-08-31 18:28:57 +0700

    refactor: improve code structure and readability in main function

--- main.cpp
+++ main.cpp
@@ -1,54 +1,39 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main (){
+int main() {
+    // Bagian 1: Data Diri
     cout << "hello" << endl;
-    cout << endl; //space
-    cout << 123;
+    cout << endl; // space
+    cout << 123 << endl;
+    cout << " Nama Saya = Arman Tri Fajri" << endl << endl;
     cout << endl;
-    cout << " Nama Saya =";
-    cout << "Arman Tri Fajri" << endl <<endl;
-    cout << endl;
-    cout << "NIM =";
-    cout << "12345678910";
-    cout << endl;
+    cout << "NIM = 12345678910" << endl;
     cout << "Saya maba PNJ" << endl;
+    
     int usia = 18;
-    usia = 19;
-    cout<< "Usia = " << usia << endl;
-    const float phi = 3.14;
+    usia = 19; // Mengubah nilai variabel usia
+    cout << "Usia = " << usia << endl;
+    
+    const float phi = 3.14; // Deklarasi konstanta
     cout << "nilai phi = " << phi << endl;
+    cout << "-----------------------------------" << endl;
+
+    // Bagian 2: Penjumlahan Variabel
+    int x = 10;
+    int y = 20;
+    x = 20; // Mengubah x sebelum z dihitung agar hasilnya konsisten
+    int z = x + y; // z = 20 + 20
+
+    cout << "nilai x di tambah nilai y = " << x << " + " << y << " = " << z << endl;
+    cout << "-----------------------------------" << endl;
+
+    // Bagian 3: Perkalian Konstanta
+    const int a = 5;
+    const int b = 2;
+    int c = a * b; 
+    cout << "hasil perkalian antara nilai " << a << " dikali dengan nilai " << b << " adalah = " << c << endl; 
+
     return 0;  
-}
-int main() {
-    int x = 10;
-    int y = 20;
-    int z = x + y;
-
-    x = 20;
-
-    cout << "nilai x di tambah nilai y = " << x << "+" << y <<"=" << z << endl;
-
-    const int a = 5;
-    const int b = 2;
-    int c = a * b; 
-    cout << "hasil penjumlahan antara nilai " << a << " dikali dengan nilai " << b << " adalah = " << c; 
-
-}
-
-int main() {
-    int x = 10;
-    int y = 20;
-    int z = x + y;
-
-    x = 20;
-
-    cout << "nilai x di tambah nilai y = " << x << "+" << y <<"=" << z << endl;
-
-    const int a = 5;
-    const int b = 2;
-    int c = a * b; 
-    cout << "hasil penjumlahan antara nilai " << a << " dikali dengan nilai " << b << " adalah = " << c; 
-
 }
