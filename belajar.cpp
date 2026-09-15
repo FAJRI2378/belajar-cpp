5cc1445f4bbf2d647c917006d572e364508fc688
FAJRI2378 <armanfajri008@gmail.com>
2026-09-15 21:09:47 +0700

    Refactor code structure for improved readability and maintainability

---
+++ belajar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    string nama;
+    cout << "masukkan nama anda : ";
+    cin >> nama;
+    cout << "nama anda adalah : " << nama << "\n";
+
+    
+    return 0;
+}
