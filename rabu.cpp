f6697c27b54ceba2f510957af9ee592c69da921e
FAJRI2378 <armanfajri008@gmail.com>
2026-09-16 10:12:04 +0700

    fix: update variable types to strings and add additional user input fields

--- rabu.cpp
+++ rabu.cpp
@@ -2,30 +2,55 @@
 using namespace std;
  // nama lengkap, nama panggilan nim, program studi, jurusan
  int main (){
-    char nama[15];
-    char panggilan[10];
-    int nim;
-    char prodi[5];
-    char jurusan[10];
+    string nama;
+    string panggilan;
+    string nim;
+    string prodi;
+    string jurusan;
+    string email;
+    string no_hp;
+    string grade;
+    float ipk;
+    float nilaiakhir;
+    bool status;
+
+
     cout << "masukkan nama lengkap anda : ";
-    cin >> nama;
+    getline(cin, nama);
     cout << "masukkan nama panggilan anda : ";
-    cin >> panggilan;
+    getline(cin, panggilan);
     cout << "masukkan nim anda : ";
     cin >> nim;
     cout << "masukkan program studi anda : ";
     cin >> prodi;
     cout << "masukkan jurusan anda : ";
     cin >> jurusan;
-
+    cout << "masukkan email anda : ";
+    cin >> email;
+    cout << "masukkan nomor handphone anda : ";
+    cin >> no_hp;
+    cout << "masukkan grade anda : ";
+    cin >> grade;
+    cout << "masukkan ipk anda : ";
+    cin >> ipk;
+    cout << "masukkan nilai akhir anda : ";
+    cin >> nilaiakhir;
+    cout << "masukkan status anda : ";
+    cin >> status;
 
     cout << "===============================" << "\n";
-    cout << "Biodata Mahasiswa" << "\n";
+    cout << "Biodata Mahasiswa" << "\n" << "\n";
     cout << "nama lengkap anda adalah : " << nama << "\n";
     cout << "nama panggilan anda adalah : " << panggilan << "\n";
     cout << "nim anda adalah : " << nim << "\n";
     cout << "program studi anda adalah : " << prodi << "\n";
     cout << "jurusan anda adalah : " << jurusan << "\n";
+    cout << "email anda adalah : " << email << "\n";
+    cout << "nomor handphone anda adalah : " << no_hp << "\n";
+    cout << "grade anda adalah : " << grade << "\n";
+    cout << "ipk anda adalah : " << ipk << "\n";
+    cout << "nilai akhir anda adalah : " << nilaiakhir << "\n";
+    cout << "status anda adalah : " << status << "\n";
     cout << "===============================" << "\n";
     
     return 0;
